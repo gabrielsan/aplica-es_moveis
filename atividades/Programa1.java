1fff1a48fe5d3191947ce3d31d9201debda7d41b
gabrielsan <75080094+gabrielsan@users.noreply.github.com>
2021-11-17 11:43:17 -0300

    PrimeiroGit

--- atividades/Programa1.java
+++ atividades/Programa1.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Atividade1 {
+public class Programa1 {
     public static void main(String args[]) throws IOException {
         FileInputStream pastadoarquivo = new FileInputStream("C:\\Users\\Mariza\\Desktop\\Testes\\Inputs.txt");
         InputStreamReader ainput = new InputStreamReader(pastadoarquivo);
